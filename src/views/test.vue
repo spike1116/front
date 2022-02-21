9949dc286a2400bc1a1f825398941131b4b845cd
wanglei <wanglei@raisecom.com>
2022-02-21 16:27:29 +0800

    test

--- src/views/test.vue
+++ src/views/test.vue
@@ -1,9 +1,20 @@
 <template>
-       <el-table :data="tableData" stripe style="width: 100%">
+<div>
+    <div style="height: 5%;border-bottom:1px solid #ccc;margin:10px;">
+        <el-input style="width:10%"></el-input>
+        <el-button  style="margin-left:10px">搜索</el-button>
+        <el-button>添加</el-button>
+    </div>
+    <div>
+<el-table :data="tableData" stripe style="width: 100%">
     <el-table-column prop="date" label="Date"  />
     <el-table-column prop="name" label="Name"  />
     <el-table-column prop="address" label="Address" />
   </el-table> 
+    </div>
+
+</div>
+       
 
 </template>
 
